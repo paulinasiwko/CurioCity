75b2645640cc9466f414fd5c9462236907284c1e
Paulina Siwko <pau.siwko@gmail.com>
2024-01-19 20:22:25 +0100

    added new js file with fetch for flickr API

---
+++ assets/js/flickr.js
@@ -0,0 +1,30 @@
+const imagesApiKey = `1f4988b7218778a8ade4218c335534cd`;
+
+$("#searchButton").on("click", function(e) {
+    e.preventDefault();
+
+    const tag = $("#cityInput").val().trim();
+
+    getImage(tag);
+})
+
+function getImage(tagName) {
+    const queryURL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${imagesApiKey}&tags=${tagName}&format=json&nojsoncallback=1`;
+
+    fetch(queryURL)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            const imageServer = data.photos.photo[10].server;
+            const imageId = data.photos.photo[10].id;
+            const imageSecret = data.photos.photo[10].secret;
+
+            const imageURL = `https://live.staticflickr.com/${imageServer}/${imageId}_${imageSecret}_w.jpg`
+            
+            const displayImage = $("<img>").attr("src", imageURL)
+            $("#imageHere").append(displayImage);
+
+        })
+}
+
