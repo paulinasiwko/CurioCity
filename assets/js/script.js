24d5aada60a16004148c92b541c6079be5f9646e
Paulina Siwko <pau.siwko@gmail.com>
2024-01-23 20:33:09 +0100

    changes cities in the array

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,21 +12,15 @@ document.addEventListener('DOMContentLoaded', function() {
   
     if(luckyButton) {
         luckyButton.addEventListener('click', function() {  
-          var cities = [
-      'Tokyo', 'Delhi', 'Shanghai', 'Sao Paulo', 'Mumbai', 'Beijing', 'Cairo', 'Dhaka', 
-      'Mexico City', 'Osaka', 'Karachi', 'Bangalore', 'New York', 'London', 'Bangkok', 
-      'Kolkata', 'Istanbul', 'Buenos Aires', 'Chongqing', 'Lagos', 'Rio de Janeiro', 'Tianjin', 
-      'Kinshasa', 'Guangzhou', 'Los Angeles', 'Moscow', 'Shenzhen', 'Lahore', 'Bangalore', 
-      'Paris', 'Jakarta', 'Lima', 'Chennai', 'Seoul', 'Hyderabad', 'London', 'Tehran', 
-      'Bogota', 'Ho Chi Minh City', 'Hong Kong', 'Baghdad', 'Fuzhou', 'Changsha', 'Wuhan', 
-      'Hanoi', 'Chengdu', 'Kuala Lumpur', 'Santiago', 'Ahmedabad', 'Singapore', 'Shantou', 
-      'Ankara', 'Yangon', 'Saint Petersburg', 'Sydney', 'Casablanca', 'Melbourne', 'Abidjan', 
-      'Alexandria', 'Kolkata', 'Surat', 'Johannesburg', 'Dar es Salaam', 'Shenyang', 
-      'Warsaw', 'Nairobi', 'Riyadh', 'Hangzhou', 'Miami', 'Quanzhou', 'Kunming', 'Zhengzhou', 
-      'New Taipei City', 'Chicago', 'Chittagong', 'Taipei', 'Toronto', 'Luanda', 'Khartoum', 
-      'Pune', 'Berlin', 'Jeddah', 'Madrid', 'Kyiv', 'Guadalajara', 'Barcelona', 'Izmir', 
-      'Kitakyushu', 'Dalian', 'Rome', 'Busan', 'Algiers', 'Cape Town', 'Dubai', 'Montreal', 
-      'Pusan', 'Medellin', 'Athens', 'Naples', 'Detroit', 'Havana', 'Accra', 'Harare'
+          var cities = ["Paris", "Dubai", "Madrid", "Tokyo", "Amsterdam", "Berlin", "Rome",
+            "New York", "Barcelona", "London", "Singapore", "Munich", "Milan", "Seoul", "Dublin",
+            "Osaka", "Hong Kong", "Vienna", "Los Angeles", "Lisbon", "Prague", "Sydney", "Istanbul",
+            "Melbourne", "Orlando", "Frankfurt", "Florence", "Taipei", "Toronto", "Athens", "Zurich",
+            "Bangkok", "Las Vegas", "Miami", "Venice", "Abu Dhabi", "Stockholm", "Brussels", "Tel Aviv",
+            "San Francisco", "Shanghai", "Warsaw", "Copenhagen", "Nice", "Budapest", "Valencia", "Mexico City",
+            "Antalya", "Beijing", "Edinburgh", "Porto", "Jerusalem", "Montreal", "Rhodes", "Verona",
+            "Lima", "Phuket", "Delhi", "Cairo", "Oslo", "Bangladore", "Chicago", "Helsinki", "Boston",
+            "Moscow", "Casablanca"
           ];
           var randomCity = cities[Math.floor(Math.random() * cities.length)];
           window.location.href = 'landing.html?city=' + encodeURIComponent(randomCity);
