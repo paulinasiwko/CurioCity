cd1ffc7db59748ec25bb3a495eb0834b6d783710
TomixJasina <thomas.jasinski90@gmail.com>
2024-01-24 19:48:56 +0000

    Updated displayRecipe function to show recipe if there was a problem with fetch

--- assets/js/edamam.js
+++ assets/js/edamam.js
@@ -16,6 +16,8 @@ function getFood(cityName) {
 }
 
 function displayRecipe(food) {
+
+    if (food.hits && food.hits.length > 0) {
     const recipeName = food.hits[3].recipe.label;
     const image = food.hits[3].recipe.image;
     const recipeLink = food.hits[3].recipe.url;
@@ -39,8 +41,31 @@ function displayRecipe(food) {
         ingredientEl.textContent = ingredient;
         foodDiv.append(ingredientEl);
     }
-}
+} else {
 
+    const foodDiv = document.getElementById("food");
+
+     foodDiv.innerHTML = `
+     <h5 class="card-title">Food that goes great with the movie</h5>
+     <h5 class="foodTitle">Pizza Margherita</h5>
+     <p><h6>Cuisine type:</h6> Italian </p>
+     <img src="assets/img/pizza.png" alt="Pizza Margherita" class="foodImg">
+     <h6>Shopping list: </h6>
+     <ul>
+         <li>300g strong bread flour</li>
+         <li>1 tsp istant yeast</li>
+         <li>1 tsp salt</li>
+         <li>1 tbsp olive oil</li>
+         <li>100 ml passata</li>
+         <li>fresh basil</li>
+         <li>1 garlic glove</li>
+         <li>125g ball mozzarella</li>
+         <li>shaved parmesan</li>
+         <li>handful of cherry tomatoes</li>
+     </ul>
+ `;
+}
+}
 $(document).ready(function() {
     var params = new URLSearchParams(window.location.search);
     var city = params.get('city');
