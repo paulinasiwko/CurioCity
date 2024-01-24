efab9976323650227b1ab07b5044a3c81bfae5b6
Paulina Siwko <pau.siwko@gmail.com>
2024-01-24 21:25:44 +0100

    updated recipe section to display pizza recipe for cities that doesn't show any recipe

--- assets/js/edamam.js
+++ assets/js/edamam.js
@@ -55,24 +55,28 @@ function displayRecipe(food) {
     const foodDiv = document.getElementById("food");
 
      foodDiv.innerHTML = `
+
      <h5 class="card-title">Food that goes great with the movie</h5>
      <h5 class="foodTitle">Pizza Margherita</h5>
-     <p><h6>Cuisine type:</h6> Italian </p>
-     <img src="assets/img/pizza.png" alt="Pizza Margherita" class="foodImg">
-     <h6>Shopping list: </h6>
-     <ul>
-         <li>300g strong bread flour</li>
-         <li>1 tsp istant yeast</li>
-         <li>1 tsp salt</li>
-         <li>1 tbsp olive oil</li>
-         <li>100 ml passata</li>
-         <li>fresh basil</li>
-         <li>1 garlic glove</li>
-         <li>125g ball mozzarella</li>
-         <li>shaved parmesan</li>
-         <li>handful of cherry tomatoes</li>
-     </ul>
- `;
+     <p><h6>Cusine type:</h6> Italian</p>
+     <div class="food-container">
+        <img src="assets/img/pizza.png" alt="Pizza Margherita" class="pizzaImage">
+         <div>
+             <h6>Shopping list: </h6>
+             <div id="list">
+             <li>300g strong bread flour</li>
+             <li>1 tsp istant yeast</li>
+             <li>1 tsp salt</li>
+             <li>1 tbsp olive oil</li>
+             <li>100 ml passata</li>
+             <li>fresh basil</li>
+             <li>1 garlic glove</li>
+             <li>125g ball mozzarella</li>
+             <li>shaved parmesan</li>
+             <li>handful of cherry tomatoes</li>    
+             </div>
+         </div>
+     </div> `;
 }
 }
 $(document).ready(function() {
